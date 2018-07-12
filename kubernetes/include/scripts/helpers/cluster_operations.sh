52e98d044f9855de0066a991d857e109c98e2771
Carlos Nunez <dev@carlosnunez.me>
2018-07-12 13:00:00 -0500

    [include/scripts/helpers/cluster_operations.sh]  |  Add a function for deploying manifests.

---
+++ kubernetes/include/scripts/helpers/cluster_operations.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+deploy_manifest_to() {
+  node_ip_address="${1?Please provide a node to send this manifest to.}"
+  manifest="${2?Please provide a manifest YAML.}"
+
+  temp_file=$(mktemp /tmp/manifest.XXXXXXXX)
+  if ! _copy_matching_files_to_home_directory_on_remote_host "$node_ip_address" "$temp_file"
+  then
+    >&2 echo "ERROR: Failed to copy manifest to $node_ip_address"
+    return 1
+  fi
+  rm -f "$temp_file"
+  command_to_run="kubectl apply --kubeconfig admin.kubeconfig -f $(basename $temp_file)"
+  if ! _run_command_on_single_kubernetes_node "$node_ip_address" "$command_to_run"
+  then
+    >&2 echo "ERROR: Kubectl command failed on $node_ip_address; see log for details."
+    return 1
+  fi
+}
--- kubernetes/include/scripts/helpers/ssh.bash
+++ kubernetes/include/scripts/helpers/ssh.bash
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+_run_command_on_single_kubernetes_node() {
+  node_address="${1?Please provide a node address.}"
+  command="${2?Please provide a command to run.}"
+  ssh_command="ssh -i ${SSH_PRIVATE_KEY_PATH} \
+    -o StrictHostKeyChecking=no \
+    -o UserKnownHostsFile=/dev/null \
+    -o LogLevel=ERROR \
+    -o User=${SSH_USER_NAME} \
+    /usr/bin/env bash -c \"${command}\""
+  if ! eval "$ssh_command"
+  then
+    >&2 echo "Failed to run this command on [$node_address]: $command"
+    return 1
+  fi
+}
+
+
 _run_command_on_all_kubernetes_controllers() {
   command="${1?Please provide the command to run.}"
   ip_addresses=$(echo "$KUBERNETES_CONTROLLERS_PUBLIC_IP_ADDRESSES" | \
