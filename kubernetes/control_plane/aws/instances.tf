274483ee98ef2520d91381e072aed180089b95ca
Carlos Nunez <dev@carlosnunez.me>
2018-07-12 18:54:19 -0500

    [control_plane/aws/instances.tf]  |  Use autoscaling groups instead of individual spot instances to fix registration timeout issues.

--- kubernetes/control_plane/aws/instances.tf
+++ kubernetes/control_plane/aws/instances.tf
@@ -3,51 +3,67 @@ resource "aws_key_pair" "kubernetes_cluster" {
   public_key = "${var.kubernetes_cluster_public_key}"
 }
 
-resource "aws_spot_instance_request" "kubernetes_control_plane" {
-  count = "${var.number_of_masters_per_control_plane}"
-  spot_price = "${var.kubernetes_nodes_spot_price}"
+resource "aws_launch_configuration" "kubernetes_control_plane" {
+  name = "kubernetes_control_plane"
+  image_id = "${var.kubernetes_node_ami}"
   instance_type = "${var.kubernetes_node_instance_type}"
-  ami = "${var.kubernetes_node_ami}"
-  availability_zone = "${element(local.availability_zones_to_use, count.index)}"
-  ebs_optimized = false
   key_name = "${aws_key_pair.kubernetes_cluster.key_name}"
-  vpc_security_group_ids = [ "${aws_security_group.kubernetes_clusters.id}" ]
-  subnet_id = "${element(aws_subnet.kubernetes_control_plane.*.id, count.index)}"
+  security_groups = [ "${aws_security_group.kubernetes_clusters.id}" ]
   associate_public_ip_address = true
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 32
+    delete_on_termination = true
+  }
+  spot_price = "${var.kubernetes_nodes_spot_price}"
   tags = "${merge(local.aws_tags, local.kubernetes_tags, var.kubernetes_control_plane_tags)}"
-  wait_for_fulfillment = "true"
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 32
-    delete_on_termination = true
-  }
 }
 
-output "kubernetes_control_plane_ip_addresses" {
-  value = "${aws_spot_instance_request.kubernetes_control_plane.*.public_ip}"
-}
-
-resource "aws_spot_instance_request" "kubernetes_workers" {
-  count = "${var.number_of_zones * var.number_of_workers_per_cluster}"
-  spot_price = "${var.kubernetes_nodes_spot_price}"
+resource "aws_launch_configuration" "kubernetes_workers" {
+  name = "kubernetes_workers"
+  image_id = "${var.kubernetes_node_ami}"
   instance_type = "${var.kubernetes_node_instance_type}"
-  ami = "${var.kubernetes_node_ami}"
-  availability_zone = "${element(local.availability_zones_to_use, count.index)}"
-  ebs_optimized = false
   key_name = "${aws_key_pair.kubernetes_cluster.key_name}"
-  vpc_security_group_ids = [ "${aws_security_group.kubernetes_clusters.id}" ]
-  subnet_id = "${element(aws_subnet.kubernetes_workers.*.id, count.index)}"
+  security_groups = [ "${aws_security_group.kubernetes_clusters.id}" ]
   associate_public_ip_address = true
-  tags = "${merge(local.aws_tags, local.kubernetes_tags, var.kubernetes_worker_tags)}"
-  wait_for_fulfillment = "true"
   root_block_device {
     volume_type = "gp2"
     volume_size = 32
     delete_on_termination = true
   }
+  spot_price = "${var.kubernetes_nodes_spot_price}"
+  tags = "${merge(local.aws_tags, local.kubernetes_tags, var.kubernetes_control_plane_tags)}"
 }
 
-output "kubernetes_workers_ip_addresses" {
-  value = "${aws_spot_instance_request.kubernetes_workers.*.public_ip}"
+resource "aws_autoscaling_group" "kubernetes_control_plane" {
+  name = "kubernetes_control_plane"
+  max_size = "${var.number_of_masters_per_control_plane}"
+  min_size = "${var.number_of_masters_per_control_plane}"
+  launch_configuration = "${aws_launch_configuration.kubernetes_control_plane.name}"
+  vpc_zone_identifier = [ "${aws_subnet.kubernetes_control_plane.*.id}" ]
+  target_group_arns = [ "${aws_lb_target_group.kubernetes_control_plane.arn}" ]
+  tags = "${merge(local.aws_tags, local.kubernetes_tags, var.kubernetes_control_plane_tags)}"
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances"
+  ]
 }
 
+resource "aws_autoscaling_group" "kubernetes_workers" {
+  name = "kubernetes_workers"
+  max_size = "${var.number_of_workers_per_cluster}"
+  min_size = "${var.number_of_workers_per_cluster}"
+  launch_configuration = "${aws_launch_configuration.kubernetes_control_plane.name}"
+  vpc_zone_identifier = [ "${aws_subnet.kubernetes_workers.*.id}" ]
+  tags = "${merge(local.aws_tags, local.kubernetes_tags, var.kubernetes_worker_tags)}"
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances"
+  ]
+}
