6d5d15adc746231874ecd966e839ac68084e602d
Carlos Nunez <dev@carlosnunez.me>
2018-07-12 19:05:36 -0500

    [include/scripts/get_cluster_addresses.sh]  |  Add a script to get cluster IP addresses given that autoscaling groups can change initially

---
+++ kubernetes/include/scripts/get_cluster_addresses.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+AWSCLI_DOCKER_IMAGE="${AWSCLI_DOCKER_IMAGE:-anigeo/awscli}"
+if [ -z "$ENV_FILE" ] || [ ! -f "$ENV_FILE" ]
+then
+  >&2 echo "WARNING: No .env file was provided. Using local environment instead."
+else
+  export $(grep -v '^#' "$ENV_FILE" | xargs)
+fi
+
+docker run --volume "$HOME/.aws:/root/.aws" \
+  --env-file "$ENV_FILE" \
+  "$AWSCLI_DOCKER_IMAGE" \
+  ec2 describe-instances \
+    --filter "Name=kubernetes_node_type,Values=master" \
+    --filter "Name=kubernetes_node_type,Values=worker" \
+    --query "Reservations[*].Instances[*].PublicIpAddress"
